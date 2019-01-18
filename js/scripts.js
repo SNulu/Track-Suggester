7bc1560ebb9f6fa0b3142c5ef9b68c4f7adcd05f
Snulu <Nnabwami@gmail.com>
2019-01-18 13:11:47 -0800

    Add branching if, else if, else conditions for trackpath in js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,45 @@ $(document).ready(function() {
     var question1Input = $("input#question1").val();
     var question2Input = $("input#question2").val();
     var question3Input = $("input#question3").val();
-    var question5Input = $("input#question4").val();
-    var question6Input = $("input#question5").val();
+    var question4Input = $("input#question4").val();
+    var question5Input = $("input#question5").val();
+
+    // $("question1").text("");
+    // $("question2").text("");
+    // $("question3").text("");
+    // $("question4").text("");
+    // $("question5").text("");
+
+    var trackPath = ""
+
+    if (question1Input === "Ruby") {
+      trackPath = "Front-End Developer"
+    } else {
+      trackPath = "Back-End Developer"
+    }
+
+    if (question4Input === "designing") {
+        trackPath = "Front-End Developer"
+    } else {
+      trackPath = "Back-End Developer"
+    }
 
 
-    alert('submit ' + question1Input + " " + question1Input + " " + question1Input + " " + question1Input + " + question1Input )
+    if (question5Input === "interactive web applications") {
+        trackPath = "Front-End Developer"
+    } else  if((question5Input === "high-performance applications") ||
+               (question5Input === "internal software")){
+      trackPath = "Back-End Developer"
+    }
+
+    if (question3Input === "working for a large company on business   software")
+        trackPath = "Front-End Developer"
+
+
+
+
+    alert('submit ' + question1Input + " " + question2Input + " " + question3Input + " " + question4Input + " " + question5Input)
+    alert("Your trackpath is " + trackPath)
     event.preventDefault();
   });
 });
